690b12b47d495c0d511f72ad57b01e2e81a1d08a
dhirajfx3 <dhirajfx2@24dcaea7d349>
2017-11-08 20:37:48 +0000

    Added files like cursor

---
+++ dbcursor.py
@@ -0,0 +1,5 @@
+from MySQLdb import connect
+connector=connect("dhirajfx2.mysql.pythonanywhere-services.com","dhirajfx2","dbmsproject2018","dhirajfx2$facarts")
+curs=connector.cursor()
+c=connector
+d=curs
